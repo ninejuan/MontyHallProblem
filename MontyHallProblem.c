f6d29f20dda0ca5094b3cba8a4440776971cdbef
Juan Lee <ninejuan@protonmail.com>
2024-07-11 14:05:05 +0900

    style: 색상 등 디테일 추가

--- MontyHallProblem.c
+++ MontyHallProblem.c
@@ -3,6 +3,14 @@
 #include <time.h>
 #include <Windows.h>
 
+enum {
+    black, dark_blue, dark_green, dark_sky, dark_red, dark_purple, dark_yellow, gray, dark_gray, blue, green, sky, red, para, yellow, white
+};
+
+void changeConsoleColor(unsigned char color) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
 int montyHall_simulation(int switch_doors) {
     int car_door = rand() % 3; 
     int chosen_door = rand() % 3; 
@@ -35,14 +43,14 @@ double get_percentage(int trials) {
 }
 
 void play_game() {
-    int goat_door, chosen_door, revealed_door, final_choice;
+    int goat_door, chosen_door, revealed_door, final_choice = 0;
     srand(time(NULL)); // 랜덤 시드 설정
 
     // 염소가 있는 문 랜덤 선택
     goat_door = rand() % 3 + 1;
 
     // 사용자에게 문 선택 받기
-    printf("세 개의 문 중 하나를 선택하세요 (1, 2, 3): ");
+    printf("\n세 개의 문 중 하나를 선택하세요 (1, 2, 3): ");
     scanf_s("%d", &chosen_door);
 
     // 사회자가 염소가 없는 문을 열어줌
@@ -50,10 +58,10 @@ void play_game() {
         revealed_door = rand() % 3 + 1;
     } while (revealed_door == goat_door || revealed_door == chosen_door);
 
-    printf("사회자가 %d번 문을 열었습니다. 그 문에는 염소가 없습니다.\n", revealed_door);
+    printf("출제자가 %d번 문을 열었습니다. 그 문에는 염소가 없습니다.\n", revealed_door);
 
     // 사용자가 선택을 바꿀지 물어보기
-    printf("선택을 바꾸시겠습니까? (1: 예, 0: 아니오): ");
+    printf("\n선택을 바꾸시겠습니까? (1: 예, 0: 아니오): ");
     int change_choice;
     scanf_s("%d", &change_choice);
 
@@ -72,17 +80,21 @@ void play_game() {
 
     // 결과 출력
     if (final_choice == goat_door) {
-        printf("축하합니다! 염소를 찾았습니다. 염소는 %d번 문에 있습니다.\n", goat_door);
+        changeConsoleColor(green);
+        printf("\n축하합니다! 염소를 찾았습니다. 염소는 %d번 문에 있었습니다.\n\n", goat_door);
     }
     else {
-        printf("아쉽습니다. 염소는 %d번 문에 있습니다. 당신의 선택은 %d번 문이었습니다.\n", goat_door, final_choice);
+        changeConsoleColor(red);
+        printf("\n아쉽습니다. 염소는 %d번 문에 있었습니다.\n\n", goat_door);
     }
 }
 
 
 int main() {
     srand(time(NULL)); 
-
+    system("cls");
+    SetConsoleDisplayMode(GetStdHandle(STD_OUTPUT_HANDLE), CONSOLE_FULLSCREEN_MODE, 0);
+    changeConsoleColor(para);
     printf(" ##  ###  #######  ##  ###  #######  ##  ###           ###  ##   ######   ##       ##               #######  ######   #######  ######    ##      #######  ##  ### \n");
     printf(" #######  ##   ##  ### ###    ###    ##  ###           ###  ##   ##  ##   ##       ##               ##   ##  ##  ##   ##   ##  ##  ##    ##      ##       ####### \n");
     printf(" #######  ##   ##  #######    ###    ##  ###           ###  ##   ##  ##   ##       ##               ##   ##  ##  ##   ##   ##  ##  ##    ##      ##       ####### \n");
@@ -91,8 +103,10 @@ int main() {
     printf(" ##  ###  ##  ###  ##  ###    ###      ###             ###  ##  ###  ##  ###      ###               ###      ### ###  ##  ###  ##  ###  ###      ###      ##  ### \n");
     printf(" ##  ###  #######  ##  ###    ###      ###             ###  ##  ###  ##  ######   ######            ###      ### ###  #######  #######  ######   #######  ##  ### \n");
     
+    changeConsoleColor(white);
+
     int choice;
-    printf("몬티홀의 역설\n\n");
+    printf("\n몬티홀의 역설\n\n");
     printf("1. 몬티홀 체험하기\n");
     printf("2. 몬티홀의 확률 구하기\n\n");
     printf("옵션을 골라주세요 >> ");
@@ -103,17 +117,20 @@ int main() {
     }
     else if (choice == 2) {
         int trials;
-        printf("시도할 횟수를 입력해주세요(권장 10,000회) >> ");
+        printf("시도할 횟수를 입력해주세요. (권장 10,000회) >> ");
         scanf_s("%d", &trials);
 
         double percentage = get_percentage(trials);
-        printf("선택을 바꿨을 때의 승률: %.2f%%\n", percentage);
+        printf("선택을 바꿨을 때의 승률: %.2f%\n", percentage);
+        printf("선택을 바꾸지 않을 때의 승률: %.2f%\n", 100.00 - percentage);
     }
     else {
+        changeConsoleColor(red);
         printf("올바르지 않은 선택입니다. \n처음으로 돌아갑니다.\n");
         Sleep(2000);
         main();
     }
-
+    changeConsoleColor(white);
+    system("PAUSE");
     return 0;
 }
